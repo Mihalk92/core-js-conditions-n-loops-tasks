9afca49b00b5c04cbe9205506ee11b98411c980e
Dmitry Kovalenko <mihalk9292@gmail.com>
2024-12-11 14:42:47 +0300

    feat: add solution 8 of 15

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -255,8 +255,14 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const numToStr = `${num}`;
+  for (let i = 0; i < numToStr.length; i += 1) {
+    if (+numToStr[i] === digit) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
